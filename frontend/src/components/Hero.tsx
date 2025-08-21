4875681153a20ca0342be41c43ffa6d344a3404f
Aarsh Roongta <aarsh.roongta@duke.edu>
2025-08-21 16:38:35 +0530

    made hero section conditional on isAuthenticated

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,8 +2,10 @@ import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function Hero() {
+  const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -11,18 +13,36 @@ export default function Hero() {
   }, []);
 
   return (
-    <div className="pt-4" data-aos="fade-up">
+    <div data-aos="fade-up">
       <div className="text-white text-center">
-        <p className="text-6xl font-extrabold">Travel Like A Local</p>
-        <p className="text-xl text-gray-300 pt-3">
-          <button
-            onClick={() => navigate("/auth")}
-            className="bg-gray-700 hover:bg-gray-600 text-white font-semibold px-4 py-1.5 mr-1 rounded-lg shadow-md transition-colors duration-200"
-          >
-            Log in
-          </button>{" "}
-          to save your favorite reels to plan your trip.
-        </p>
+        {isAuthenticated ? (
+          <>
+            <p className="text-5xl font-extrabold pt-3">
+              View your saved reels
+            </p>
+            <div className="mt-5 text-2xl">
+              <button
+                onClick={() => navigate("/saved")}
+                className="bg-gray-700 hover:bg-gray-600 text-white font-semibold px-4 py-2 rounded-lg shadow-md transition-colors duration-200"
+              >
+                Saved Reels
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className="text-6xl font-extrabold">Travel Like A Local</p>
+            <p className="text-xl text-gray-300 pt-3">
+              <button
+                onClick={() => navigate("/auth")}
+                className="bg-gray-700 hover:bg-gray-600 text-white font-semibold px-4 py-1.5 mr-1 rounded-lg shadow-md transition-colors duration-200"
+              >
+                Log in
+              </button>{" "}
+              to save your favorite reels to plan your trip.
+            </p>
+          </>
+        )}
       </div>
     </div>
   );
